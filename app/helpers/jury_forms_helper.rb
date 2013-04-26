3b6ec60e2e2a298d351f3184783ab3534a48d85e
Hanz Heukeshoven <hanzheukeshoven@ubuntu.(none)>
2013-04-25 22:46:08 -0500

    User Story: B3 (As a judge I want to view past juries)

--- app/helpers/jury_forms_helper.rb
+++ app/helpers/jury_forms_helper.rb
@@ -1,2 +1,24 @@
 module JuryFormsHelper
+
+	def form_information(jury_forms, judge)
+		out = ""
+		jury_forms.each do |jury_form|
+			if jury_form.judge == judge
+				out << "<p><b>Student:</b>"<< jury_form.jury.student.name << "</p>"
+
+				out <<"<p><b>Criteria:</b></p>"
+
+				jury_form.measures.each do |measure|
+					out << "<p><b>" << measure.name << "</b>: " 
+					out << measure.score.to_s << "</p>"
+				end 
+				out << "</p>"
+				out << "<p>" << "<b>Comments:</b>"
+				out << jury_form.comments << "</p>"
+			end
+		end
+
+		out.html_safe
+	end
+
 end

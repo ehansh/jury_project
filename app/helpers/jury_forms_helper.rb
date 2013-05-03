bf075d212d4058e8b1193eb6030b277b5227d053
Hanz Heukeshoven <hanzheukeshoven@ubuntu.(none)>
2013-05-02 19:43:37 -0500

    Added Repertoire to the form_information method in JuryFormsHelper

--- app/helpers/jury_forms_helper.rb
+++ app/helpers/jury_forms_helper.rb
@@ -14,7 +14,7 @@ module JuryFormsHelper
 	def form_information(jury_form)
 		out = ""
 		out << "<p><b>Student:</b>"<< jury_form.jury.student.name << "</p>"
-
+		out << "<p><b>Repertoire:</b>"<< AppliedStudyForm.find(jury_form.applied_study_form_id).repertoires.first.title << "</p>"
 		out <<"<p><b>Criteria:</b></p>"
 
 		jury_form.measures.each do |measure|

9d6d45606800ec9024820e8efaa790824cec265a
Hans <hans@ubuntu.ubuntu-domain>
2014-04-05 21:59:14 -0500

    Adding in a modal-button to Applied Study Forms to make applied_study_forms index page better

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -19,4 +19,34 @@ module StudentsHelper
 		out << "</table>"
 		out.html_safe	
 	end
+
+	def applied_study_forms_module_buttons(student)
+		student.applied_study_forms.first.nil?
+			out = ""
+			out << '<tr id="first"><td>'
+			out << student.name
+			out << " has no applied study forms</td>"
+		else
+			student.applied_study_forms.each do |applied_study_form|
+				out = %Q[
+					<!-- Button to trigger modal -->
+				    <td><a href="#myModal" role="button" class="btn" data-toggle="modal">Launch demo modal</a></td>
+				     
+				    <!-- Modal -->
+				    <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				    <div class="modal-header">
+				    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				    <h3 id="myModalLabel">Modal header</h3>
+				    </div>
+				    <div class="modal-body">
+				    <p>One fine body…</p>
+				    </div>
+				    <div class="modal-footer">
+				    <button class="btn btn-primary" data-dismiss="modal" aria-hidden="true">Close</button>
+				    </div>
+				    </div>
+				]
+			out.html_safe
+		end
+	end
 end
